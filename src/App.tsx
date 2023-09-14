434f1c5674de46c73e874fea52f106f7c2f2a030
Awais Abbas <awaisabbas47@gmail.com>
2023-09-14 19:50:38 +0500

    Add support for horizontal scroll component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { MaskImageCursorAnimation } from '@/components/maskImageCursorAnimation';
+import { HorizontalScrollAnimation } from '@/components/horizontalScrollAnimation';
 
 function App() {
   return (
     <>
       <MaskImageCursorAnimation />
+      <HorizontalScrollAnimation />
+      <div className="flex h-screen items-center justify-center bg-darker-grey">
+        <span className="font-semibold uppercase text-neutral-500">
+          More Animations Coming Soon :)
+        </span>
+      </div>
     </>
   )
 }
